72e4206a34febbe51e083525e242b1f8319d3b8b
Arcado-vol2 <piterz31a@gmail.com>
2022-05-11 14:46:14 +0300

    fixed my govnocode 1.1

--- code/world_logic/curved_line.gd
+++ code/world_logic/curved_line.gd
@@ -4,13 +4,18 @@ extends Node
 
 
 var rng = RandomNumberGenerator.new()
-var canvas = []
 onready var tile_map = $TileMap
 
 
 func parabolla(point1, point2):
-	var b = (point2[1] - point1[1])/(0.5*point2[0] + pow(point1[0],2)/(2*point2[0]) - point1[0])
-	var a = -b/(2*point2[0])
+	var b
+	var a
+	if point1.y == point2.y:
+		b = 0
+		a = 0
+	else:
+		b = (point2[1] - point1[1])/(0.5*point2[0] + pow(point1[0],2)/(2*point2[0]) - point1[0])
+		a = -b/(2*point2[0])
 	var c = point1[1]  - pow(point1[0],2)*a -  point1[0]*b
 	return [a,b,c]
 
@@ -59,7 +64,7 @@ func draw_ray2(point1, point2):
 		#print(x," ",y)
 		tile_map.set_cell(x,y,0)
 
-func draw_ray(p1,p2):
+func draw_ray(p1,p2, radius = 0):
 	var x1 = p1[0]
 	var y1 = p1[1]
 	var x2 = p2[0]
@@ -88,6 +93,15 @@ func draw_ray(p1,p2):
 	var y = y1
 	var error = el/2
 	var t = 0   
+	
+	if pdx == 1:
+		for i in radius:
+			tile_map.set_cell(x,y+i,0)
+			tile_map.set_cell(x,y-i,0)
+	else:
+		for i in radius:
+			tile_map.set_cell(x+i,y,0)
+			tile_map.set_cell(x-i,y,0)
 	tile_map.set_cell(x,y,0)
 	
 	while t < el:
@@ -100,6 +114,15 @@ func draw_ray(p1,p2):
 			x += pdx
 			y += pdy
 		t += 1
+		
+		if pdx == 1:
+			for i in radius:
+				tile_map.set_cell(x,y+i,0)
+				tile_map.set_cell(x,y-i,0)
+		else:
+			for i in radius:
+				tile_map.set_cell(x+i,y,0)
+				tile_map.set_cell(x-i,y,0)
 		tile_map.set_cell(x,y,0)
 		
 		
@@ -149,13 +172,17 @@ func suboptimal_path(start_point, end_point, start_range, end_range):
 	for i in range(1,len(points)-1):
 		tmp_points.append(points[i])
 		
-		var mx = max(points[i][1], points[i+1][1])
-		var mn = min(points[i][1], points[i+1][1])
-		var d = abs(mn - mx)
 		
-		tmp_points.append(Vector2(
-			rng.randi_range(points[i][0]+int(x_split[i]*0.2), points[i+1][0]-int(x_split[i]*0.2)),
-			rng.randi_range(mn+int(d*0.2),mx-int(d*0.2)) ))
+		var max_y = max(points[i][1], points[i+1][1])
+		var min_y = min(points[i][1], points[i+1][1])
+		var d_y = abs( max_y - min_y)
+		var d_x = abs(min(points[i][0], points[i+1][0]) - max(points[i][0], points[i+1][0]))
+		
+		if false:
+			tmp_points.append(Vector2(
+				rng.randi_range(points[i][0]+int(d_x*0.2), points[i+1][0]-int(d_x*0.2)),
+				rng.randi_range(min_y+int(d_y*0.2),max_y-int(d_y*0.2)) ))
+		tmp_points.append(Vector2(points[i][0] + int(d_x*rng.randf_range(0.35,0.65)), min_y + int(d_y*rng.randf_range(0.35,0.65))  ))
 	tmp_points.append(points[len(points)-1])
 	points = tmp_points.duplicate()
 	
@@ -180,12 +207,11 @@ func suboptimal_path(start_point, end_point, start_range, end_range):
 		var p
 		for x in range(points[i-1][0],points[i][0]+1):
 			p = Vector2(x,int( a*pow(x,2)+b*x+c ))
-			draw_ray(last_point, p)
-			print(p, last_point)
+			draw_ray(last_point, p, 7)
 			last_point = p
 			
 
 
 func _on_Button_pressed():
-	var tmp = [1, 30, 70,50, 30, 10]
+	var tmp = [1, 30, 80,70, 30, 10]
 	suboptimal_path(Vector2(tmp[0], tmp[1]), Vector2(tmp[2], tmp[3]), tmp[4], tmp[5])
