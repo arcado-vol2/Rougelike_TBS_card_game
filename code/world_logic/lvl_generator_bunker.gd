85ce54326a553f6196d4169622bf0ea001f9bd45
fross72 <fross724@gmail.com>
2022-12-06 21:57:39 +0300

    fixed bunker corridors and made bunker walls

---
+++ code/world_logic/lvl_generator_bunker.gd
@@ -0,0 +1,45 @@
+extends YSort
+var prefabs
+var path
+var BSP_script= load("res://code/world_logic/BSP_script.gd")
+var Cellular_script = load("res://code/world_logic/BSP_+_cellular.gd")
+var BuildingPath_script = load("res://code/world_logic/Building_Path.gd")
+var CurvedLine_script = load("res://code/world_logic/curved_line.gd")
+var rng = RandomNumberGenerator.new()
+var startRoom
+var rooms_array
+
+onready var CreateRooms = $create_rooms
+onready var floor_tilemap = $floor
+onready var wals_tilemap = $wals
+onready var astar_navigation_tilemap = $astar_nav_mesh
+onready var rooms_node = $floor/rooms
+
+func _createRooms():
+	CreateRooms.setRooms(rooms_node)
+	CreateRooms.setMap(floor_tilemap, astar_navigation_tilemap)
+	CreateRooms.generate()
+	yield(CreateRooms, "generation_complete")
+	for y in 300:
+		for x in 300:
+			if floor_tilemap.get_cell(x, y) == 0:
+				astar_navigation_tilemap.set_cell(x,y,0)
+			if (floor_tilemap.get_cell(x-150,y-150)==-1):
+				wals_tilemap.set_cell(x-150,y-2-150,0)
+	wals_tilemap.update_bitmask_region()
+	floor_tilemap.update_bitmask_region()
+func placeStart():
+	rooms_array = rooms_node.get_children()
+	startRoom = rooms_array[rng.randi_range(0,rooms_array.size())]
+	
+
+func _ready():
+	_createRooms()
+func get_prefabs():
+	return prefabs
+
+
+func _on_create_rooms_generation_complete():
+	#placeStart()
+	#print(path.get_closest_point(startRoom.mapPosition))
+	get_parent().get_parent().start()
--- code/world_logic/lvl_generator_cave.gd
+++ code/world_logic/lvl_generator_cave.gd
@@ -1,23 +1,19 @@
 extends YSort
 var prefabs
+var path
 var BSP_script= load("res://code/world_logic/BSP_script.gd")
 var Cellular_script = load("res://code/world_logic/BSP_+_cellular.gd")
 var BuildingPath_script = load("res://code/world_logic/Building_Path.gd")
 var CurvedLine_script = load("res://code/world_logic/curved_line.gd")
 var rng = RandomNumberGenerator.new()
+var startRoom
+var rooms_array
 
 onready var CreateRooms = $create_rooms
 onready var floor_tilemap = $floor
 onready var wals_tilemap = $wals
 onready var astar_navigation_tilemap = $astar_nav_mesh
 onready var rooms_node = $floor/rooms
-
-enum location {
-	cave,
-	bunker,
-	none,
-}
-export var location_type = location.cave
 
 func _BSP():
 	rng.randomize()
@@ -30,11 +26,11 @@ func _BSP():
 	BSP.split()
 	var room=$Area
 	drawBSP(room)
-	yield(get_tree().create_timer(6),"timeout")
+	#yield(get_tree().create_timer(6),"timeout")
 	var positions =[]
 	for cave in (rooms_node.get_children()):
 		positions.append(Vector2(cave.start.x+cave.width/2,cave.start.y+cave.height/2))
-	var path = BuildingPath_script.find_mst(positions)
+	path = BuildingPath_script.find_mst(positions)
 	var ref = funcref(curvedLine,"suboptimal_path")
 	BuildingPath_script.drawCorridors(ref,path,rooms_node)
 #	Короче, тут Влад, удаляем всё и пишём лучше, вообще нужно подумать
@@ -62,17 +58,6 @@ func _BSP():
 #	а вот конец
 	
 	_on_create_rooms_generation_complete()
-#	хз зачем это хранить, но храни, если хочешь, хотя я бы удалил
-#	var corridors =[]
-#	for cave in $Rooms.get_children():
-#		var position = Vector2(cave.start.x+cave.width/2,cave.start.y+cave.height/2)
-#		var p = path.get_closest_point(position)
-#		for connection in path.get_point_connections(p):
-#			if not connection in corridors:
-#				var start = path.get_point_position(p)
-#				var end = path.get_point_position(connection)
-#				curvedLine.suboptimal_path(start,end,5,5,2)
-#			corridors.append(p)
 
 func drawBSP(room):
 	if room.isLeaf():
@@ -88,27 +73,19 @@ func drawBSP(room):
 		if(room.rightRoom!=null):
 			drawBSP(room.rightRoom)
 
-func _cell():
-	var t = Cellular_script.new(Vector2.ZERO, 20,20,0.5,2)
-	t.setMap(floor_tilemap)
-	t.createRoom()
-
-func _createRooms():
-	CreateRooms.setRooms(rooms_node)
-	CreateRooms.setMap(floor_tilemap, astar_navigation_tilemap)
-	CreateRooms.generate()
+func placeStart():
+	rooms_array = rooms_node.get_children()
+	startRoom = rooms_array[rng.randi_range(0,rooms_array.size())]
+	
 
 func _ready():
-	match location_type:
-		location.cave:
-			_BSP()
-		location.bunker:
-			_createRooms()
-	
+	_BSP()
 
 func get_prefabs():
 	return prefabs
 
 
 func _on_create_rooms_generation_complete():
+	placeStart()
+	print(path.get_closest_point(startRoom.mapPosition))
 	get_parent().get_parent().start()
